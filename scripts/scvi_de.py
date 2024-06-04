6c6c022c8922811dc000bc8ab91798f41b4ed07f
amsesk <ubuntu@ip-10-128-254-37.ec2.internal>
2024-06-04 15:42:13 +0000

    update to match obj

--- scripts/scvi_de.py
+++ scripts/scvi_de.py
@@ -1,23 +1,27 @@
-import pmbi.wrappers.scvi as pmbint
-import argparse
-import importlib
 import os
-importlib.reload(pmbint)
-
-def parse_comparisons(string):
-    comparisons = []
-    for pair in string.split(';'):
-        comparisons.append(tuple(pair.split(',')))
-    return comparisons
+import argparse
+import pmbi.wrappers.scvi as pmbscvi
+import pathlib
+import pdb
+import scipy.sparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--data", help = "Name of input h5ad/h5mu file.")
-parser.add_argument("-m", "--model", help = "Path to model directory.")
-parser.add_argument("-g", "--groupby", help = "Obs column to group by.")
-parser.add_argument("-f", "--factorial", action = "store_true", help = "Pass flag to run deg analysis for factorial combinations of groupby categories.")
-parser.add_argument("-c", "--comparisons",  help = "Pass flag to run deg analysis for factorial combinations of groupby categories.")
-parser.add_argument("--fdr_target",  default=0.05, type = float, help = "FDR cutoff for differential expression.")
+parser.add_argument("-a", "--adata", help = "Name of input h5ad file.")
+parser.add_argument("-m", "--model", help = "Path to trained, saved model directory.")
+parser.add_argument("-g", "--groupby", help = "Variable in AnnData.obs to group cells by.")
+parser.add_argument("-f", "--factorial", action = "store_true", help = "Pass flag to test factorial combinations of the unique values of groupby variable.")
+parser.add_argument("-o", "--output_path", required = True, help = "Output path to write the table of differentially expressed genes.")
+parser.add_argument("--fdr_target", type = float, required = False, default = 0.05, help = "FDR target to pass to SCVI.differential_expression.")
 args = parser.parse_args()
 
-sm = pmbint.ScviModeler.load(args.data, args.model)
-sm.differential_expression(groupby=args.groupby, factorial = args.factorial, fdr_target=args.fdr_target, comparisons = parse_comparisons(args.comparisons))
+output_path = args.output_path or f"./{os.path.basename(args.model)}_deg.csv"
+modeler = pmbscvi.ScviModeler(pathlib.Path(args.adata))
+modeler.load_model(pathlib.Path(args.model))
+deg = modeler.differential_expression(
+        groupby = args.groupby,
+        factorial = args.factorial,
+        comparisons = None,
+        fdr_target = args.fdr_target,
+        )
+deg = deg.reset_index(names="gene")
+deg.to_csv(output_path, sep = ",", index = False)
