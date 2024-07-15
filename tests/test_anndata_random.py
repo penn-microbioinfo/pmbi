2dc8075b862b87359da263d97b11f496c58c7aa3
amsesk <amsesk@upenn.edu>
2024-07-15 20:12:41 +0000

    add tests

--- tests/test_anndata_random.py
+++ tests/test_anndata_random.py
@@ -12,3 +12,14 @@ def test_random_barcodes_assert_unique():
     bcs = pmbi.anndata.random.random_barcodes(n = 1024, length = 5, assert_unique = True)
     assert len(bcs) == len(set(bcs))
 
+# %%
+def test_random_barcodes_all_match_pattern():
+    bcs = pmbi.anndata.random.random_barcodes(n = 1024, length = 5, assert_unique = True)
+    assert all([re.match(BARCODE_PATTERN, bc) for bc in bcs])
+
+def test_random_adata():
+    adata = pmbi.anndata.random.random_adata(shape = (10, 10), nobs_cols=5, nvar_cols=5)
+    assert adata.shape == (10,10)
+    assert adata.obs.shape == (10,5)
+    assert adata.var.shape == (10,5)
+    assert isinstance(adata, anndata.AnnData)
