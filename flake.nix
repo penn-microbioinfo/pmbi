484240f3d4234dcd0160f1106126ed94f10a50c7
amsesk <amsesk@umich.edu>
2024-10-07 13:07:00 +0000

    make work

--- flake.nix
+++ flake.nix
@@ -1,29 +1,35 @@
 {
-  description = "A Nix-flake-based development environment for pmbi";
+    description = "A Nix-flake-based development environment for python3.12";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self , nixpkgs ,... }: let
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        neovim
-        sqlite
-        python3
-        nodejs_22
-        poetry
-      ];
-
-      shellHook = ''
-        echo "python `${pkgs.python3}/bin/python --version`"
-      '';
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nvimenv.url = "path:/home/amsesk/.config/home-manager/FHSEnv";
     };
-  };
+    outputs = { self, nixpkgs, nvimenv}:
+        let
+        system = "x86_64-linux";
+    pkgs = nixpkgs;
+    in 
+    {
+        devShells."${system}".default = let
+            pkgs = import nixpkgs {
+                inherit system;
+            };
+            env = (pkgs.buildFHSUserEnv {
+                name = "pmbi-pipzone";
+                targetPkgs = pkgs: (with pkgs; [
+                        python312
+                        python312Packages.pip
+                        python312Packages.virtualenv
+                        # cudaPackages.cudatoolkit
+                ]);
+                runScript = "bash";
+                }).env; 
+                    in pkgs.mkShell {
+            inputsFrom = [ 
+                env
+                nvimenv.devShells."${system}".default
+                ];
+        };
+    };
 }
