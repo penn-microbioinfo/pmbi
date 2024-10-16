b012913d19b75e119e9fc336fe76ad750d018554
amsesk <amsesk@upenn.edu>
2024-10-16 23:16:14 +0000

    finish

--- scripts/fasta_replace_degenerate_nucleotides.py
+++ scripts/fasta_replace_degenerate_nucleotides.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 import argparse
 import io
+from numpy.random import randint
 
 # %%
 parser =argparse.ArgumentParser()
@@ -8,18 +9,27 @@ parser.add_argument("fasta", action = "store", help = "Path to FASTA file.")
 args = parser.parse_args()
 
 degen = {
-        ""
+        "A": ["A"],
+        "G": ["G"],
+        "C": ["C"],
+        "T": ["T"],
+        "R": ["A", "G"],
+        "Y": ["C", "T"],
+        "S": ["G", "C"],
+        "W": ["A", "T"],
+        "K": ["G", "T"],
+        "M": ["A", "C"],
+        "B": ["C", "G", "T"],
+        "D": ["A", "G", "T"],
+        "H": ["A", "C", "T"],
+        "V": ["A", "C", "G"],
+        "N": ["A", "G", "C", "T"]
         }
 # %%
-f = io.StringIO(">test\nATCKMWSYRATCG")
-seqs = []
 for record in SeqIO.parse(args.fasta, "fasta"):
-    print(f"{record.id}\t{len(record.seq)}")
-
-# %%
-SeqIO.
-seqs[0].molecule_type
-    seqs.append(record)
-
-# %%
+    new = ""
+    for char in record.seq:
+        new += degen[char.upper()]
+    record.seq = new
+    print(record.format("fasta"))
 
